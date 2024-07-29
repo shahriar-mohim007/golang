4ef3357d95bf50195d7b2c414f1b92f29174e695
shahriar-mohim007 <nasifshahriar4@gmail.com>
2024-07-29 20:18:49 +0600

    golang

--- struct.go
+++ struct.go
@@ -1,98 +1,99 @@
-// Maps are a convenient way to store some kinds of data, but they have
-// limitations. They don’t define an API since there’s no way to constrain a
-// map to only allow certain keys. Also, all of the values in a map must be of the same type. For these reasons, maps are not an ideal way to pass data
-// from function to function. When you have related data that you want to group together, you should define a struct.
+// // Maps are a convenient way to store some kinds of data, but they have
+// // limitations. They don’t define an API since there’s no way to constrain a
+// // map to only allow certain keys. Also, all of the values in a map must be of the same type. For these reasons, maps are not an ideal way to pass data
+// // from function to function. When you have related data that you want to group together, you should define a struct.
 package main
-import {
 
-}
-// You can define a
-// struct type inside or outside of a function. A struct type that’s defined
-// within a function can only be used within that function.
-type person struct {
-	name string
-	age int
-	pet string
-}
-func main(){
-	var fred person
-	bob := person{}
-	julia := person{
-		"Julia",
-		40,
-		"cat",
-	}
-	beth := person{
-		age: 30,
-		name: "Beth",
-	}
-	bob.name = "Bob"
-    fmt.Println(beth.name)
-	//Anonymous Structs
-	var person struct {
-		name string
-		age int
-		pet string
-	}
-	person.name = "bob"
-	person.age = 50
-	person.pet = "dog"
-	pet := struct {
-	name string
-	kind string
-	}{
-	name: "Fido",
-	kind: "dog",
-	}
-
-//There are two common situations where
-// anonymous structs are handy. The first is when you translate external data
-// into a struct or a struct into external data (like JSON or protocol buffers).
-// This is called marshaling and unmarshaling data. We’ll learn how to do this
-// in “encoding/json”.
-//Go does allow you to perform a type
-// conversion from one struct type to another if the fields of both structs have
-// the same names, order, and types.
-type firstPerson struct {
-	name string
-	age int
-}
-type secondPerson struct {
-	name string
-	age int
-}
-
-type firstPerson struct {
-	name string
-	age int
-}
-	f := firstPerson{
-	name: "Bob",
-	age: 50,
-}
-	var g struct {
-	name string
-	age int
-}
-	// compiles -- can use = and == between identical named and anonymous structs
-	g = f
-	fmt.Println(f == g)
-
-
-// 	If all the fields of a struct are comparable, the struct itself is comparable, so two expressions of
-// that type may be compared using == or !=. The == operation compares the corresponding
-// fields of the two structs in order, so the two printed expressions below are equivalent:
-type Point struct{ X, Y int }
-p := Point{1, 2}
-q := Point{2, 1}
-fmt.Println(p.X == q.X && p.Y == q.Y) // "false"
-fmt.Println(p == q)
-// "false"
-// Comparable struct types, like other comparable types, may be used as the key type of a map.
-type address struct {
-hostname string
-port int
-}
-hits := make(map[address]int)
-hits[address{"golang.org", 443}]++
-}
+//
+//import "fmt"
+//
+//// You can define a
+//// struct type inside or outside of a function. A struct type that’s defined
+//// within a function can only be used within that function.
+//type person struct {
+//	name string
+//	age int
+//	pet string
+//}
+//func main(){
+//	var fred person
+//	bob := person{}
+//	julia := person{
+//		"Julia",
+//		40,
+//		"cat",
+//	}
+//	beth := person{
+//		age: 30,
+//		name: "Beth",
+//	}
+//	bob.name = "Bob"
+//    fmt.Println(beth.name)
+//	//Anonymous Structs
+//	var person struct {
+//		name string
+//		age int
+//		pet string
+//	}
+//	person.name = "bob"
+//	person.age = 50
+//	person.pet = "dog"
+//	pet := struct {
+//	name string
+//	kind string
+//	}{
+//	name: "Fido",
+//	kind: "dog",
+//	}
+//
+////There are two common situations where
+//// anonymous structs are handy. The first is when you translate external data
+//// into a struct or a struct into external data (like JSON or protocol buffers).
+//// This is called marshaling and unmarshaling data. We’ll learn how to do this
+//// in “encoding/json”.
+////Go does allow you to perform a type
+//// conversion from one struct type to another if the fields of both structs have
+//// the same names, order, and types.
+//type firstPerson struct {
+//	name string
+//	age int
+//}
+//type secondPerson struct {
+//	name string
+//	age int
+//}
+//
+//type firstPerson struct {
+//	name string
+//	age int
+//}
+//	f := firstPerson{
+//	name: "Bob",
+//	age: 50,
+//}
+//	var g struct {
+//	name string
+//	age int
+//}
+//	// compiles -- can use = and == between identical named and anonymous structs
+//	g = f
+//	fmt.Println(f == g)
+//
+//
+//// 	If all the fields of a struct are comparable, the struct itself is comparable, so two expressions of
+//// that type may be compared using == or !=. The == operation compares the corresponding
+//// fields of the two structs in order, so the two printed expressions below are equivalent:
+//type Point struct{ X, Y int }
+//p := Point{1, 2}
+//q := Point{2, 1}
+//fmt.Println(p.X == q.X && p.Y == q.Y) // "false"
+//fmt.Println(p == q)
+//// "false"
+//// Comparable struct types, like other comparable types, may be used as the key type of a map.
+//type address struct {
+//hostname string
+//port int
+//}
+//hits := make(map[address]int)
+//hits[address{"golang.org", 443}]++
+//}
--- topics/erros/format_string.go
+++ topics/erros/format_string.go
@@ -1,44 +1,46 @@
 // A convenient way to format strings in Go is by using the standard library's fmt.Sprintf() function.
-// It's a string interpolation function, similar to JavaScript's built-in template literals. 
+// It's a string interpolation function, similar to JavaScript's built-in template literals.
 // The %v substring uses the type's default formatting, which is often what you want.
+package main
 
 const name = "Kim"
 const age = 22
-s := fmt.Sprintf("%v is %v years old.", name, age)
-// s = "Kim is 22 years old."
-//Rounding floats
 
-fmt.Printf("I am %f years old", 10.523)
-// I am 10.523000 years old
-
-// The ".2" rounds the number to 2 decimal places
-fmt.Printf("I am %.2f years old", 10.523)
-// I am 10.52 years old
-
-package main
-
-import (
-	"fmt"
-)
-
-func getSMSErrorString(cost float64, recipient string) string {
-	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent",
-		cost,
-		recipient,
-	)
-}
-
-// don't edit below this line
-
-func test(cost float64, recipient string) {
-	s := getSMSErrorString(cost, recipient)
-	fmt.Println(s)
-	fmt.Println("====================================")
-}
-
-func main() {
-	test(1.4, "+1 (435) 555 0923")
-	test(2.1, "+2 (702) 555 3452")
-	test(32.1, "+1 (801) 555 7456")
-	test(14.4, "+1 (234) 555 6545")
-}
+//s := fmt.Sprintf("%v is %v years old.", name, age)
+//// s = "Kim is 22 years old."
+////Rounding floats
+//
+//fmt.Printf("I am %f years old", 10.523)
+//// I am 10.523000 years old
+//
+//// The ".2" rounds the number to 2 decimal places
+//fmt.Printf("I am %.2f years old", 10.523)
+//// I am 10.52 years old
+//
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func getSMSErrorString(cost float64, recipient string) string {
+//	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent",
+//		cost,
+//		recipient,
+//	)
+//}
+//
+//// don't edit below this line
+//
+//func test(cost float64, recipient string) {
+//	s := getSMSErrorString(cost, recipient)
+//	fmt.Println(s)
+//	fmt.Println("====================================")
+//}
+//
+//func main() {
+//	test(1.4, "+1 (435) 555 0923")
+//	test(2.1, "+2 (702) 555 3452")
+//	test(32.1, "+1 (801) 555 7456")
+//	test(14.4, "+1 (234) 555 6545")
+//}
